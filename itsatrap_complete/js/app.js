351ad9cd7afb89dc4bf71ad4d3cfb8542be862e0
Michael Lorenzo <Mikeronomicon@gmail.com>
2015-10-22 12:51:56 -0700

    new version using two different jquery methods to hide and show

--- itsatrap_complete/js/app.js
+++ itsatrap_complete/js/app.js
@@ -1,3 +1,12 @@
+//long winded version
 function showButton() {
 	$(".warning").hide().show("slow");
 }
+
+$(document).ready(showButton);
+
+//anonymous function version
+
+$(function() {
+	$(".warning").hide().show("slow");
+});
