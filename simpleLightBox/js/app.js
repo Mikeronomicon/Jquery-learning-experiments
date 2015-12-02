60181ea1e453bd0c706b3b90031324e3d29c771e
Michael <Mikeronomicon@gmail.com>
2015-12-02 15:19:02 -0800

    creating overlay logic for displaying images

--- simpleLightBox/js/app.js
+++ simpleLightBox/js/app.js
@@ -1,5 +1,11 @@
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+//append overlay
+$("body").append($overlay);
+
+//capture click event on a link to an image
 $("#imageGallery a").click(function(event) {
   event.preventDefault();
   var href = $(this).attr("href");
-  console.log(href);
+  $overlay.show();
 });
