eb5a44b074f32ecc6682c2f56430158c0499908e
Michael <Mikeronomicon@gmail.com>
2015-12-02 13:30:41 -0800

    spoiler can be revealed and button is now removed when pressed

--- spoilerRevealer/js/app.js
+++ spoilerRevealer/js/app.js
@@ -1,2 +1,8 @@
+//initially hide spoiler
 $(".spoiler span").hide();
+//append button to spoiler class
 $(".spoiler").append("<button>Reveal Spoiler!</button>");
+$("button").click(function() {
+  $(".spoiler span").show();
+  $(this).remove()
+});
