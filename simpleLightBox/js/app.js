6241a3800ce77769bb591c2529e3230b9c172d7b
Michael <Mikeronomicon@gmail.com>
2015-12-02 15:34:31 -0800

    basic lightbox done, adding alt text to img

--- simpleLightBox/js/app.js
+++ simpleLightBox/js/app.js
@@ -1,11 +1,27 @@
 var $overlay = $('<div id="overlay"></div>');
 var $image = $("<img>");
+var $caption = $('<p></p>');
+
+//add image to overlay
+$overlay.append($image);
+
+//show img alt text in overlay
+$overlay.append($caption);
+
 //append overlay
 $("body").append($overlay);
 
 //capture click event on a link to an image
 $("#imageGallery a").click(function(event) {
   event.preventDefault();
-  var href = $(this).attr("href");
+  var imagePath = $(this).attr("href");
+  //update overlay with the selected image
+  $image.attr("src", imagePath);
+
   $overlay.show();
 });
+
+//hide overlay when you click again
+$overlay.click(function() {
+  $overlay.hide();
+})
