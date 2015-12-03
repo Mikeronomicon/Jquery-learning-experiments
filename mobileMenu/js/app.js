25af0dfaa51053420fa81c21c72af67f8186884e
Michael <Mikeronomicon@gmail.com>
2015-12-03 13:55:39 -0800

    menu works, needs tweaking to look better

--- mobileMenu/js/app.js
+++ mobileMenu/js/app.js
@@ -1,4 +1,18 @@
 //create a select menu and append to #menu
 var $select = $("<select></select>");
 
+//create and append select menu
 $("#menu").append($select);
+
+//cycle over menu links
+$("#menu a").each(function() {
+  var $anchor = $(this);
+  //create an option
+  var $option = $("<option></option>");
+  //option's value is the href
+  $option.val($anchor.attr("href"));
+  //option's text is title of link
+  $option.text($anchor.text());
+  //append option to select menu
+  $select.append($option);
+});
