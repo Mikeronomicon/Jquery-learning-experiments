30aa1fb81131cb6127585dc59d34a87023a3e2de
Michael <Mikeronomicon@gmail.com>
2015-12-03 15:07:49 -0800

    refactoring code to be easier to read

--- passwordConfirmationForm/js/app.js
+++ passwordConfirmationForm/js/app.js
@@ -4,8 +4,16 @@ var $confirmPassword = $("#confirm_password");
 //hide hints when password hits 8 characters
 $("form span").hide();
 
+function isPasswordValid() {
+  return $password.val().length > 8
+}
+
+function arePasswordsMatching() {
+  return $password.val() === $confirmPassword.val();
+}
+
 function passwordEvent() {
-  if ($password.val().length > 8) {
+  if (isPasswordValid()) {
     $password.next().hide();
   } else {
     $password.next().show();
@@ -13,7 +21,7 @@ function passwordEvent() {
 }
 
 function confirmPasswordEvent() {
-  if ($password.val() === $confirmPassword.val()) {
+  if (arePasswordsMatching()) {
     $confirmPassword.next().hide();
   } else {
     $confirmPassword.next().show();
